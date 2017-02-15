38a600ca66b18aa6f6403ed18ac21ffd5ca67068
Salma Ghaouzy <ghaouzysalma@gmail.com>
2017-02-15 16:27:10 +0100

    ajout main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//==============================
+//    INCLUDES
+//==============================
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "Envir.h"
+#include "Case.h"
+#include "Bacterie.h"
+#include "BactA.h"
+#include "BactB.h"
+
+
+//==============================
+//    FUNCTION DeCLARATION
+//==============================
+
+//==============================
+//    MAIN
+//==============================
