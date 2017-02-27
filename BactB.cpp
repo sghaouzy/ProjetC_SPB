293f7fd49ba9b625fd7bb707148f03679f9fa055
pdenailly <paul.denailly@laposte.net>
2017-02-27 15:33:29 +0100

    Update BactB.cpp

--- BactB.cpp
+++ BactB.cpp
@@ -2,6 +2,7 @@
 //    INCLUDES
 //==============================
 #include "BactB.h"
+#include "BactA.h"
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -13,13 +14,35 @@
 //==============================
 //    CONSTRUCTORS
 //==============================
+BactB::BactB(){
+	w_=Cout_;
+	type="S";
+}
 
 
 //==============================
 //    DESTRUCTOR
 //==============================
+BactB::~BactB(){
+}
 
 //==============================
 //    PUBLIC METHODS
 //==============================
 
+bool BactB::death(){
+	float nombre = 0;
+        srand(time(NULL));
+        nombre = (float)rand() / (float)RAND_MAX;
+	if (nombre<=0.02) return true;
+	else return false;
+}
+
+
+void BactB::adaptation(float elt_nutritif){
+	Bout_ = elt_nutritif-elt_nutritif*RBB;
+	B_ = B_+elt_nutritif*RBB-B_*RBC;
+	Cout_ = Cout_+B_*RBC;
+        w_ = Cout_;
+	if (w_< Wmin) w_=0;
+}
