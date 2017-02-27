69ff5e8f5e7721de3291015bafffd4462e3ff172
pdenailly <paul.denailly@laposte.net>
2017-02-27 15:29:06 +0100

    Update BactA.cpp

--- BactA.cpp
+++ BactA.cpp
@@ -4,6 +4,7 @@
 #include "BactA.h"
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 //==============================
@@ -11,6 +12,13 @@
 //==============================
 
 //==============================
+//    CONSTRUCTORS
+//==============================
+BactA::BactA(){
+	w_=Bout_;
+	type="L";
+}
+=======
 //    CONSTRUCTOR
 //==============================
 
@@ -19,7 +27,28 @@
 //    DESTRUCTOR
 //==============================
 
+BactA::~BactA(){
+}
+
 //==============================
 //    PUBLIC METHODS
 //==============================
+
+bool BactA::death(){
+	float nombre = 0;
+        srand(time(NULL));
+        nombre = (float)rand() / (float)RAND_MAX;
+	if (nombre<=0.02) return true;
+	else return false;
+}
+
+
+void BactA::adaptation(float elt_nutritif){
+	Aout_ = elt_nutritif-elt_nutritif*RAA;
+	A_ = A_+elt_nutritif*RAA-A_*RAB;
+	Bout_ = Bout_+A_*RAB;
+        w_ = Bout_;
+	if (w_< Wmin) w_=0;
+}
+
 
