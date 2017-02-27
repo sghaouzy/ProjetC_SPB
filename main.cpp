1e26681a36a1ffdda95908479bf500d9694ee96b
pdenailly <paul.denailly@laposte.net>
2017-02-27 15:50:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 #include <cstdio>
 #include <cstdlib>
-
+#include <iostream>
+#include <vector>
 #include "Envir.h"
 #include "Case.h"
 #include "Bacterie.h"
@@ -19,3 +20,17 @@
 //==============================
 //    MAIN
 //==============================
+
+int main(int argc, char*argv[]){
+
+//Initialisation de l'environnement 
+Envir envir = Envir();
+
+//Initialisation des bactéries
+envir.init_bact();
+
+//Lancement d'un pas de temps de simulation
+envir.run();
+
+  return EXIT_SUCCESS;
+}
