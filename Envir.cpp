620b21f6ecab5e464f00dd7d0052528016208cf4
pdenailly <paul.denailly@laposte.net>
2017-03-05 14:53:35 +0100

    Remise en forme de Envir.cpp avec les initialisation de tableaux en 2D

--- Envir.cpp
+++ Envir.cpp
@@ -8,6 +8,10 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <algorithm>    // std::random_shuffle
+#include <vector>       // std::vector
+
+using namespace std;
 
 //==============================
 //    DEFINITION STATIC ATTRIBUTES
@@ -18,10 +22,20 @@
 //==============================
 Envir::Envir(){
 	longueur = 10;
+	largeur = 5;
 	A_init_ = 50;
-	Bacterie *list_bact[longueur];
-	Case *list_case[longueur];
 	D = 0.1;
+	list_bact = new Bacterie**[longueur];
+
+	for(int it=0; it<longueur; it++){
+		list_bact[it] = new Bacterie*[largeur];		
+	}
+
+	list_case = new Case**[longueur];
+
+	for(int it=0; it<longueur; it++){
+		list_case[it] = new Case*[largeur];		
+	}
 }
 //==============================
 //    DESTRUCTOR
@@ -30,35 +44,52 @@ Envir::~Envir(){
 }
 
 
-
-//==============================
-//    DESTRUCTOR
-//==============================
-
 //==============================
 //    PUBLIC METHODS
 //==============================
 void Envir::init_bact(){
-	list_bact[0] = new BactA();
-	for(int it=0; it<=longueur/2-1; it++){
-		list_bact[2*it] = new BactA();
-		list_bact[2*it+1] = new BactB();		
-	}
-	printf("%f\n",list_bact[8]->Aout());
 	
+//Première étape : créer le tableau 2 dimensions de 0 et de 1 et le mélanger 
+	// random generator function:
+int limite=0;
+vector<vector<int> > grille;
+for (int x=0;x<longueur;x++){
+	limite+=1;
+	if (limite<longueur/2)
+		grille.push_back(vector<int>(largeur,0));   
+        else
+		grille.push_back(vector<int>(largeur,1));
+}
+  
+  // using built-in random generator:
+random_shuffle (grille.begin(), grille.end() );
+
+
+//Deuxième étape : remplir le tableau de bactéries aléatoirement. Si la position x,y correspond à 1 dans la grille aléatoire alors
+//il sera crée un nouveau pointeur sur une BactA, sinon ce sera une BactB.
+	
+for(int x=0; x<longueur; x++){
+	for(int y=0; y<largeur;y++){
+			
+			if (grille[x][y]==1){
+				list_bact[x][y] = new BactA();
+			}
+
+			else {
+				list_bact[x][y] = new BactB();
+			}
+		
+	}	
+}
 }
 
-void Envir::sup_bact(){
-
-	for(int it=1; it<longueur; it++){
-		delete list_bact[it];
-	}
-}
 
 
 void Envir::init_case(){
-	for(int it=0; it<=longueur; it++){
-		list_case[it] = new Case();
+	for(int x=0; x<longueur; x++){
+		for(int y=0; y<largeur;y++){
+			list_case[x][y] = new Case();
+		}
 	}
 }
 
@@ -103,11 +134,18 @@ void Envir::sup_case(){
 
 
 void Envir::run(){
-	for(int it=0; it<=longueur/2-1;it++){
+//Fonction à compléter, ici on teste la survie des bactéries et leur adaptation aux nouvelles concentrations. Il faut finaliser
+//la fonction de diffusion et l'inclure dans la fonction run.
+	for(int x=0; x<longueur;x++){
+		for(int y=0;y<largeur;y++){
 		//Mort des bactéries
-		if (list_bact[it]->death()==1) delete list_bact[it];
-		if (list_bact[it]->type_()=="L") list_bact[it]->adaptation(A_init_);
-		else list_bact[it]->adaptation(0);
+        		if (list_bact[x][y]->death()==1) delete list_bact[x][y];
+			else{
+         			if (list_bact[x][y]->type_()==1) list_bact[x][y]->adaptation(list_case[x][y]->Aout());
+                		else list_bact[x][y]->adaptation(list_case[x][y]->Bout());
+				printf("%f %f %f %f\n", list_bact[x][y]->type_(), list_bact[x][y]->Aout(),list_bact[x][y]->A(), list_bact[x][y]->w());
+			}
+		}
 	}
 }
 
